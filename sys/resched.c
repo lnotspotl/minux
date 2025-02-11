fe1c6aeb4a6338046bf9e78ecd75c83e7deaeeee
lnotspotl <lnotspotl@gmail.com>
2025-02-11 13:56:01 -0500

    Add age based scheduler

--- sys/resched.c
+++ sys/resched.c
@@ -5,13 +5,13 @@
 #include <proc.h>
 #include <q.h>
 
-int current_sched_class = XINUSCHED;
-extern int disable(short *);
-extern int restore(short *);
+int current_sched_class = AGESCHED;
 
 LOCAL int resched_age();
 LOCAL int resched_linux();
 LOCAL int resched_xinu();
+
+LOCAL int project_priority(int prio, int min, int max);
 
 unsigned long currSP;	/* REAL sp of current process */
 extern int ctxsw(int, int, int, int);
@@ -35,6 +35,56 @@ int resched()
 		default:
 			return SYSERR; // should never happen
 	}
+}
+
+int resched_age() {
+	register struct pentry *optr;  /* pointer to old process entry */
+	register struct pentry *nptr;  /* pointer to new process entry */
+	int pid;
+	int next_prio;
+
+	// Increment the priority of all processes in the ready queue
+	for (pid = q[rdyhead].qnext; pid != rdytail; pid = q[pid].qnext) {
+
+		// Null process should be ignored
+		if(pid == 0) {
+			continue;
+		}
+
+		// Increment priority and project it to valid range
+		q[pid].qkey = project_priority(q[pid].qkey + 2, MINPRIO, MAXPRIO);
+	}
+
+	// Check the priority of the currently running process. 
+	// No need to perform context switch if it has the highest priority
+	// If equal, perform context switch (round robin)
+	optr = &proctab[currpid];
+	if (optr->pstate == PRCURR && lastkey(rdytail) < optr->pprio) {
+		return (OK);
+	}
+
+	// Change the state of the current process to PRREADY and insert it into the ready queue
+	if(optr->pstate == PRCURR) {
+		optr->pstate = PRREADY;
+		insert(currpid, rdyhead, optr->pprio);
+	}
+
+	// Remove the highest priority process at the end of the ready queue
+	currpid = getlast(rdytail);
+	nptr = &proctab[currpid];
+
+	// Mark this new process as currently running
+	nptr->pstate = PRCURR;  
+
+#ifdef RTCLOCK
+	preempt = QUANTUM; // reset preemption counter
+#endif
+
+	// Perform context switch
+	ctxsw((int)&optr->pesp, (int)optr->pirmask, (int)&nptr->pesp, (int)nptr->pirmask);
+
+	// All done! Success
+	return OK;
 }
 
 int resched_xinu() {
@@ -73,14 +123,26 @@ int resched_linux() {
 	return OK;
 }
 
-int resched_age() {
-	return OK;
-}
-
 void setschedclass(int sched_class) {
 	current_sched_class = sched_class;
 }
 
 int getschedclass() {
 	return current_sched_class;
+}
+
+int project_priority(int priority, int min, int max) {
+
+	// If priority is greater than max, return max
+	if(priority > max) {
+		return max;
+	}
+
+	// If priority is less than min, return min
+	if(priority < min) {
+		return min;
+	}
+
+	// Otherwise, return priority
+	return priority;
 }
