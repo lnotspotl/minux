e757830d8baa60217a5f35578ad56ca3a66d243d
lnotspotl <lnotspotl@gmail.com>
2025-02-11 14:12:09 -0500

    Format resched.c

--- hw02_procsched/sys/resched.c
+++ hw02_procsched/sys/resched.c
@@ -13,7 +13,7 @@ LOCAL int resched_xinu();
 
 LOCAL int project_priority(int prio, int min, int max);
 
-unsigned long currSP;	/* REAL sp of current process */
+unsigned long currSP; /* REAL sp of current process */
 extern int ctxsw(int, int, int, int);
 /*-----------------------------------------------------------------------
  * resched  --  reschedule processor to highest priority ready process
@@ -23,126 +23,122 @@ extern int ctxsw(int, int, int, int);
  *			current process if other than PRREADY.
  *------------------------------------------------------------------------
  */
-int resched()
-{
-	switch (getschedclass()) {
-		case AGESCHED:
-			return resched_age();
-		case LINUXSCHED:
-			return resched_linux();
-		case XINUSCHED:
-			return resched_xinu();
-		default:
-			return SYSERR; // should never happen
-	}
+int resched() {
+  switch (getschedclass()) {
+  case AGESCHED:
+    return resched_age();
+  case LINUXSCHED:
+    return resched_linux();
+  case XINUSCHED:
+    return resched_xinu();
+  default:
+    return SYSERR; // should never happen
+  }
 }
 
 int resched_age() {
-	register struct pentry *optr;  /* pointer to old process entry */
-	register struct pentry *nptr;  /* pointer to new process entry */
-	int pid;
-	int next_prio;
+  register struct pentry *optr; /* pointer to old process entry */
+  register struct pentry *nptr; /* pointer to new process entry */
+  int pid;
+  int next_prio;
 
-	// Increment the priority of all processes in the ready queue
-	for (pid = q[rdyhead].qnext; pid != rdytail; pid = q[pid].qnext) {
+  // Increment the priority of all processes in the ready queue
+  for (pid = q[rdyhead].qnext; pid != rdytail; pid = q[pid].qnext) {
 
-		// Null process should be ignored
-		if(pid == 0) {
-			continue;
-		}
+    // Null process should be ignored
+    if (pid == 0) {
+      continue;
+    }
 
-		// Increment priority and project it to valid range
-		q[pid].qkey = project_priority(q[pid].qkey + 2, MINPRIO, MAXPRIO);
-	}
+    // Increment priority and project it to valid range
+    q[pid].qkey = project_priority(q[pid].qkey + 2, MINPRIO, MAXPRIO);
+  }
 
-	// Check the priority of the currently running process. 
-	// No need to perform context switch if it has the highest priority
-	// If equal, perform context switch (round robin)
-	optr = &proctab[currpid];
-	if (optr->pstate == PRCURR && lastkey(rdytail) < optr->pprio) {
-		return (OK);
-	}
+  // Check the priority of the currently running process.
+  // No need to perform context switch if it has the highest priority
+  // If equal, perform context switch (round robin)
+  optr = &proctab[currpid];
+  if (optr->pstate == PRCURR && lastkey(rdytail) < optr->pprio) {
+    return (OK);
+  }
 
-	// Change the state of the current process to PRREADY and insert it into the ready queue
-	if(optr->pstate == PRCURR) {
-		optr->pstate = PRREADY;
-		insert(currpid, rdyhead, optr->pprio);
-	}
+  // Change the state of the current process to PRREADY and insert it into the
+  // ready queue
+  if (optr->pstate == PRCURR) {
+    optr->pstate = PRREADY;
+    insert(currpid, rdyhead, optr->pprio);
+  }
 
-	// Remove the highest priority process at the end of the ready queue
-	currpid = getlast(rdytail);
-	nptr = &proctab[currpid];
+  // Remove the highest priority process at the end of the ready queue
+  currpid = getlast(rdytail);
+  nptr = &proctab[currpid];
 
-	// Mark this new process as currently running
-	nptr->pstate = PRCURR;  
+  // Mark this new process as currently running
+  nptr->pstate = PRCURR;
 
 #ifdef RTCLOCK
-	preempt = QUANTUM; // reset preemption counter
+  preempt = QUANTUM; // reset preemption counter
 #endif
 
-	// Perform context switch
-	ctxsw((int)&optr->pesp, (int)optr->pirmask, (int)&nptr->pesp, (int)nptr->pirmask);
+  // Perform context switch
+  ctxsw((int)&optr->pesp, (int)optr->pirmask, (int)&nptr->pesp,
+        (int)nptr->pirmask);
 
-	// All done! Success
-	return OK;
+  // All done! Success
+  return OK;
 }
 
 int resched_xinu() {
-	register struct	pentry	*optr;	/* pointer to old process entry */
-	register struct	pentry	*nptr;	/* pointer to new process entry */
+  register struct pentry *optr; /* pointer to old process entry */
+  register struct pentry *nptr; /* pointer to new process entry */
 
-	/* no switch needed if current process priority higher than next*/
+  /* no switch needed if current process priority higher than next*/
 
-	if ( ( (optr= &proctab[currpid])->pstate == PRCURR) &&
-	   (lastkey(rdytail)<optr->pprio)) {
-		return(OK);
-	}
-	
-	/* force context switch */
+  if (((optr = &proctab[currpid])->pstate == PRCURR) &&
+      (lastkey(rdytail) < optr->pprio)) {
+    return (OK);
+  }
 
-	if (optr->pstate == PRCURR) {
-		optr->pstate = PRREADY;
-		insert(currpid,rdyhead,optr->pprio);
-	}
+  /* force context switch */
 
-	/* remove highest priority process at end of ready list */
+  if (optr->pstate == PRCURR) {
+    optr->pstate = PRREADY;
+    insert(currpid, rdyhead, optr->pprio);
+  }
 
-	nptr = &proctab[ (currpid = getlast(rdytail)) ];
-	nptr->pstate = PRCURR;		/* mark it currently running	*/
-#ifdef	RTCLOCK
-	preempt = QUANTUM;		/* reset preemption counter	*/
+  /* remove highest priority process at end of ready list */
+
+  nptr = &proctab[(currpid = getlast(rdytail))];
+  nptr->pstate = PRCURR; /* mark it currently running	*/
+#ifdef RTCLOCK
+  preempt = QUANTUM; /* reset preemption counter	*/
 #endif
-	
-	ctxsw((int)&optr->pesp, (int)optr->pirmask, (int)&nptr->pesp, (int)nptr->pirmask);
-	
-	/* The OLD process returns here when resumed. */
-	return OK;
+
+  ctxsw((int)&optr->pesp, (int)optr->pirmask, (int)&nptr->pesp,
+        (int)nptr->pirmask);
+
+  /* The OLD process returns here when resumed. */
+  return OK;
 }
 
-int resched_linux() {
-	return OK;
-}
+int resched_linux() { return OK; }
 
-void setschedclass(int sched_class) {
-	current_sched_class = sched_class;
-}
+void setschedclass(int sched_class) { current_sched_class = sched_class; }
 
-int getschedclass() {
-	return current_sched_class;
-}
+int getschedclass() { return current_sched_class; }
 
 int project_priority(int priority, int min, int max) {
 
-	// If priority is greater than max, return max
-	if(priority > max) {
-		return max;
-	}
+  // If priority is greater than max, return max
+  if (priority > max) {
+    return max;
+  }
 
-	// If priority is less than min, return min
-	if(priority < min) {
-		return min;
-	}
+  // If priority is less than min, return min
+  if (priority < min) {
+    return min;
+  }
 
-	// Otherwise, return priority
-	return priority;
+  // Otherwise, return priority
+  return priority;
 }
